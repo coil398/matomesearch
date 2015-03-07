abe19b8dadd4c458f2535d67c14b6e9ebc62021f
yuya sato <omikositteiiyone@live.jp>
2015-03-07 20:49:25 +0900

    てきとう

--- matomesearch/application/views/home_index.php
+++ matomesearch/application/views/home_index.php
@@ -6,17 +6,66 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <title>タイトル</title>
+    <link rel="stylesheet" href="/css/cssreset-min.css">
+    <style>
+        html, body {
+            height: 100%;
+            min-height: 100%;
+            width: 100%;
+        }
+
+        .container {
+            height: 100%;
+            width: 100%;
+        }
+
+        .header {
+            height: 100px;
+            background: #f2f2f2;
+            margin-bottom: 30px;
+        }
+
+        .search {
+
+        }
+        
+    </style>
 </head>
 <body>
-    23.102.72.248
-    <div class="containet">
-        <div class="row">
-            <div class="col-xs-12">
+    <div class="container">
+
+        <div class="header">
+            <div class="search">
+
+                <input type="text">
 
             </div>
         </div>
+
+        <div class="main">
+            
+            todo: 検索ボックス、表示画面
+
+            <div class="result">
+                <ul class="left">
+                    <li>
+                        <a href="blogUrl">ブログ記事タイトル</a>
+                        <span class="small">元ブログ</span>
+                    </li>
+                </ul>
+
+                <ul class="right">
+                    <li>
+                        <a href="blogUrl">ブログ記事タイトル</a>
+                        <span class="small">元ブログ</span>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
     </div>
     <script src="/js/jquery-2.1.3.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    <!-- <script src="/js/bootstrap.min.js"></script> -->
 </body>
-</html>
+</html
