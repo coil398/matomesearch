46c2716ceb2e3b991b83bed39038f9b535277113
川瀬拓実 <coil_the-world_1341398@i.softbank.jp>
2015-03-07 20:45:36 +0900

    データベースまわりの設定を変更

---
+++ matomesearch/application/config/development/database.php
@@ -0,0 +1,8 @@
+<?php
+
+$active_record = FALSE;
+$db['default']['hostname'] = '23.102.72.248';
+
+
+
+?>
---
+++ web/application/views/home_index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>タイトル</title>
+</head>
+<body>
+    23.102.72.248
+    <div class="containet">
+        <div class="row">
+            <div class="col-xs-12">
+                
+            </div>
+        </div>
+    </div>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
